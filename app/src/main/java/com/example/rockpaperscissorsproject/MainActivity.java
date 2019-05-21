bdcf5eaa13d9d4cfc049c3478bcd73b5ebc869a0
Dimitrios Moutafidis <dmoutafi@csd.auth.gr>
2019-05-21 15:25:46 +0300

    Update

--- app/src/main/java/com/example/rockpaperscissorsproject/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissorsproject/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.rockpaperscissorsproject;
 
+import android.os.PersistableBundle;
+import android.service.autofill.TextValueSanitizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -13,11 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
     private Button rock, paper, scissors;
 
+    private Bundle bundle = new Bundle();
+
     private ImageView playerIcon, cpuIcon;
     
     private String playerChoise, cpuChoise;
 
-    private int player_score=0,cpu_score=0;
+    private TextView cpuScore, playerScore;
+
+    private int player_score, cpu_score;
 
     private Random random = new Random();
 
@@ -28,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
         playerIcon = findViewById(R.id.myMove);
         cpuIcon = findViewById(R.id.cpuMove);
+
+        playerScore = findViewById(R.id.playerScore);
+        playerScore.setText("Player: " + player_score);
+        cpuScore = findViewById(R.id.cpuScore);
+        cpuScore.setText("Machine: " + cpu_score);
 
         rock = findViewById(R.id.rock);
         paper = findViewById(R.id.paper);
@@ -60,10 +72,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if(savedInstanceState != null){
+            cpu_score = savedInstanceState.getInt("machineScore");
+            player_score = savedInstanceState.getInt("humanScore");
+            playerScore.setText("Player: " + player_score);
+            cpuScore.setText("Machine: " + cpu_score);
+        }
+
     }
 
     private void makeMove() {
-        int randomChoose = random.nextInt(3);
+        int randomChoose = random.nextInt(500)%3;
 
         if(randomChoose == 0){
             cpuChoise = "rock";
@@ -79,25 +98,35 @@ public class MainActivity extends AppCompatActivity {
 
 
         if(playerChoise.equals("rock") && cpuChoise.equals("scissors")){
-            player_score++;
+            playerScore.setText("Player: " + ++player_score);
             Toast.makeText(this, "You Win!", Toast.LENGTH_SHORT).show();
         } else if(playerChoise.equals("rock") && cpuChoise.equals("paper")){
-            cpu_score++;
+            cpuScore.setText("Machine: " + ++cpu_score);
             Toast.makeText(this, "You Lost!", Toast.LENGTH_SHORT).show();
         } else if(playerChoise.equals("paper") && cpuChoise.equals("rock")){
-            player_score++;
+            playerScore.setText("Player: " + ++player_score);
             Toast.makeText(this, "You Win!", Toast.LENGTH_SHORT).show();
         } else if(playerChoise.equals("paper") && cpuChoise.equals("scissors")){
-            cpu_score++;
+            cpuScore.setText("Machine: " + ++cpu_score);
             Toast.makeText(this, "You Lost!", Toast.LENGTH_SHORT).show();
         } else if(playerChoise.equals("scissors") && cpuChoise.equals("rock")){
-            cpu_score++;
+            cpuScore.setText("Machine: " + ++cpu_score);
             Toast.makeText(this, "You Lost!", Toast.LENGTH_SHORT).show();
         } else if(playerChoise.equals("scissors") && cpuChoise.equals("paper")){
-            player_score++;
+            playerScore.setText("Player: " + ++player_score);
             Toast.makeText(this, "You Win!", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("humanScore", player_score);
+        outState.putInt("machineScore", cpu_score);
+        bundle.putAll(outState);
+
     }
 }
